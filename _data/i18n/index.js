5235fb6392813df4bc55133e77478fbb5dc3727e
Sander Roesink <sander@roesink.dev>
2023-08-27 17:19:57 +0200

    Start implementing i18n

---
+++ _data/i18n/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  current_language: {
+    'nl-NL': 'Nederlands',
+    'en-US': 'English'
+  },
+  nl: {
+    'nl-NL': 'Nederlands',
+    'en-US': 'Dutch'
+  },
+  en: {
+    'nl-NL': 'Engels',
+    'en-US': 'English'
+  },
+  about_me: {
+    'nl-NL': 'Over mij',
+    'en-US': 'About me'
+  },
+  language: {
+    'nl-NL': 'Taal',
+    'en-US': 'Language'
+  }
+};
